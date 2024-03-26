03d9f6fc32d28c3d4593991c4d234e83bf04d429
S-Peanut <silvianpascaru@gmail.com>
2024-03-27 00:45:04 +0200

    Scss Modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ const { faL } = require('@fortawesome/free-solid-svg-icons');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{html,ts}",
-    ],
+    content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
     theme: {
         extend: {},
     },
     plugins: [
+        require('postcss-import'),
         require('@tailwindcss/forms'),
-    ],
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ]
 }
